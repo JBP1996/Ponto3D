fa26436a79b5ccc1b3611b179d2660e766429b0a
JBP1996 <jbp.1996@hotmail.com>
2017-03-21 15:30:27 +0000

    Utilizacao metodos - Prof. Dario

---
+++ src/ponto/Ponto.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ponto;
+
+/**
+ *
+ * @author JBP1996 Atec
+ */
+public class Ponto {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    int x, y;
+    public Ponto (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Ponto ( ) {
+        this (-1, -1); // this também pode ser usado para chamar construtores
+    }
+    public void print ( ){
+        System.out.print (x +", "+ y);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ponto3D p3= new Ponto3D();
+        p3.printPonto3D();
+    }   
+}
---
+++ src/ponto/Ponto3D.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ponto;
+
+/**
+ *
+ * @author JBP1996 Atec
+ */
+public class Ponto3D extends Ponto{
+    int z;
+    public Ponto3D (int x, int y, int z)   {
+        super(x,y);
+        this.z = z;
+    }
+    
+    public Ponto3D () {
+        this (-1, -1, -1);
+    }
+    
+    public void printPonto3D(){
+        super.print();
+        System.out.println(","+z);
+    }
+}
